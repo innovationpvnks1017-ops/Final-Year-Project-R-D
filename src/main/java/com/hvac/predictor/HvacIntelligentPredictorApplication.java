45cd7d3388b46c46a0b291a079bdbb86a8cef4ca
ri-01 <dasriyanka858@gmail.com>
2026-04-26 11:01:53 +0530

    Best project presentation ever.

---
+++ src/main/java/com/hvac/predictor/HvacIntelligentPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.hvac.predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvacIntelligentPredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvacIntelligentPredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hvac/predictor/HvacIntelligentPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvac.predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvacIntelligentPredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
